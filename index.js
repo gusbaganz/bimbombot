34316435ac7ffef02d87f9162b4b841606cb7f6d
gusbaganz <134161015+gusbaganz@users.noreply.github.com>
2023-06-21 23:47:11 -0400

    Update index.js
    updated if/else, still EST only

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 require('dotenv').config();
-
-const {
-    App
-} = require('@slack/bolt');
+ 
+const axios = require('axios');
+const { App } = require('@slack/bolt');
 const signingSecret = process.env['SLACK_SIGNING_SECRET']
 const botToken = process.env['SLACK_BOT_TOKEN']
 const app = new App({
     signingSecret: signingSecret,
     token: botToken,
 });
-
-(async() => {
+ 
+(async () => {
     await app.start(process.env.PORT || 12000);
-    app.message('?', async({
-            message,
-            say
-        }) => {
-            if (new Date().getUTCHours() > 20) {
-                say(`Hello <@${message.user}>, it's past 5pm in Rio de Janeiro. Click <https://open.spotify.com/track/45gBYIbizYRUtvVFsRIHAu?si=9d3eab54fc934411|here.>`)
-            } else {
-                say(`Sorry <@${message.user}>, back to work! It's not 5pm in Rio de Janeiro yet.`)
-            }
-        }
-
-        , );
-})
-
-console.log(`⚡️ Bolt app is running!`);;
-const tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-tomorrow.setHours(16, 0, 0);;
-(async() => {
-    const result = await client.chat.scheduleMessage({
-        channel: C05D5B91DRV,
-        text: "Clock out and click <https://open.spotify.com/track/45gBYIbizYRUtvVFsRIHAu?si=9d3eab54fc934411|here>, it's 5PM in Rio!",
-        post_at: tomorrow.getTime()
-
-    });
-})
+ 
+    app.message('?', async ({ message, say }) => {
+        if (new Date().getHours() >= 16) { say(`Clock out and click <https://open.spotify.com/track/45gBYIbizYRUtvVFsRIHAu?si=9d3eab54fc934411|here>, <@${message.user}>, it’s 5pm in Rio de Janeiro!`)} else{say (`Sorry <@${message.user}>, back to work! It's not 5pm in Rio de Janeiro yet.`)}
+;
+    })
+ 
+ 
+    console.log(`⚡️ Bolt app is running!`);
+})();
